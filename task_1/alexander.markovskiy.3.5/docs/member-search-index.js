e98e1a139c239d0db93b1fa24e22e8f66e64da93
Alex <bnc@ukr.net>
2018-02-18 12:10:17 +0200

    About My First Java Docs

---
+++ task_1/alexander.markovskiy.3.5/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"FirstProgramm","l":"calc(int, int)"},{"p":"<Unnamed>","c":"FirstProgramm","l":"FirstProgramm()"},{"p":"<Unnamed>","c":"FirstProgramm","l":"main(String[])","url":"main-java.lang.String:A-"}]
