5ed3fbc20376f775e894483a1e30021ba348fd72
Alex <bnc@ukr.net>
2018-02-18 11:47:29 +0200

    This is lesson about Java comments.Markovskiy.

---
+++ task_1/alexander.markovskiy.3.5/src/FirstProgramm.java
@@ -0,0 +1,38 @@
+/**
+* My First class
+* Param-pam-pam
+* 12345
+*/
+
+
+public class FirstProgramm {
+
+	/**
+	*
+	* @param args command line arguments.
+	*
+	*/
+
+
+	public static void main(String[] args) {
+		System.out.println("This is a first java program"); // out string text
+	}
+
+
+	/**
+	*
+	* Devide 2 values
+	*
+	* @param x integer
+	* @param y integer
+	* @since 1.7
+	* @return To be or Not to be :)
+	*/
+	public int calc(int x, int y){	
+
+/*
+Please play sound: "It's a reebok or a nike." :)
+
+*/		return x / y;
+	}
+}
